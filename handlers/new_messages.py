df0fb672e642867345610ad1edd0e32615db30a7
aleX-Xandr <72668137+aleX-Xandr@users.noreply.github.com>
2023-03-11 16:17:39 +0200

    Отредактировал оформление сообщений

--- handlers/new_messages.py
+++ handlers/new_messages.py
@@ -1,6 +1,7 @@
 from loader import client,bot
 from telethon import TelegramClient, events, utils
 from keyboards.url_keyboard import url_kbd
+from utils.tools import format_message
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
@@ -14,32 +15,41 @@ async def my_event_handler(event):
     title = None
 
     if hasattr(event.message.to_id, 'channel_id'):
-        await client.download_media(event.message.media,"./photo.jpg") 
+
         real_id, peer_type = utils.resolve_id(event.chat_id)
+        if hasattr(sender, 'username') and not sender.username is None:
+            username = '@'+sender.username
+        else:
+            username = ''
+        if hasattr(sender, 'first_name') and not sender.first_name is None:
+            first_name = sender.first_name
+        else:
+            first_name = ''
+        if hasattr(sender, 'last_name') and not sender.last_name is None:
+            last_name = sender.last_name
+        else:
+            last_name = ''
+        if hasattr(chat, 'title'):
+            title = chat.title
+
         if event.message.media:
+            await client.download_media(event.message.media,"./photo.jpg") 
             if event.message.message:
                 with open('./photo.jpg', "rb") as f:
-                    await bot.send_photo(5841914430, f,caption=event.message.message, reply_markup=url_kbd(real_id, event))
+                    await bot.send_photo(5841914430, f,caption=format_message(title, f"({first_name} {last_name}) {username}", event.message.message), reply_markup=url_kbd(real_id, event))
             else:
                 with open('./photo.jpg', "rb") as f:
                     await bot.send_photo(5841914430, f, reply_markup=url_kbd(real_id, event))
         else:
             await bot.send_message(5841914430,event.message.message, reply_markup=url_kbd(real_id, event))
+
         return
+
         real_id, peer_type = utils.resolve_id(event.chat_id)
         link = f"https://t.me/c/{real_id}/{event.id}"
 
         if event.message.message !='':
-            if hasattr(sender, 'username'):
-                username = f"{sender.username if sender.username != None else ''}"
-            if hasattr(sender, 'first_name'):
-                first_name = f"{sender.first_name if sender.first_name != None else ''}"
-
-            if hasattr(sender, 'last_name'):
-                last_name = f"{sender.last_name if sender.last_name != None else ''}"
-
-            if hasattr(chat, 'title'):
-                title = chat.title
+            
 
             x = ['message_forward', {'chat_id': event.chat_id, 'title': title, 'msg_text':event.message.message, 'username' : username, 'first_name' :first_name, 'last_name': last_name, 'link' :link}]
             print(x)
