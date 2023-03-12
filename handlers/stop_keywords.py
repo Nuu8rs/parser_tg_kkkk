da1072c984ade0e373ae12480f1a24ffe36b68f9
aleX-Xandr <72668137+aleX-Xandr@users.noreply.github.com>
2023-03-12 19:46:46 +0200

    Update stop_keywords.py
    sdfgdfgdsgfd

--- handlers/stop_keywords.py
+++ handlers/stop_keywords.py
@@ -1,13 +1,16 @@
-﻿from loader import bot,dp,storage
+from loader import bot,dp,storage
 from aiogram.dispatcher import FSMContext
 from aiogram import types
 from keyboards.stop_keywords import stop_kbd
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from utils.tools import add_keyword
+from utils.tools import add_keyword, del_keyword
 
 
-class Form(StatesGroup):
+class Form1(StatesGroup):
     stop_keyword = State()
+    
+class Form2(StatesGroup):
+    del_stop_keyword = State()
 
 
 @dp.message_handler(text=["/STOP", "/stop", "⛔️STOP слова⛔️"])
@@ -23,19 +26,33 @@ async def process_start_command(message: types.Message):
         open("keywords.txt", "x")
         msg = "У вас еще нет стоп слов"
     await message.reply(msg, reply_markup=stop_kbd())
-
+    
 @dp.callback_query_handler(lambda c: c.data == 'add_stop_keyword')
 async def process_callback_button1(call: types.CallbackQuery):
-    await Form.stop_keyword.set()
+    await Form1.stop_keyword.set()
     await call.message.answer("Введите слово или несколько слов через запятую")
 
-@dp.message_handler(state=Form.stop_keyword)
+@dp.message_handler(state=Form1.stop_keyword)
 async def process_name(message: types.Message, state: FSMContext):
     await state.finish()
     if "," in message.text:
         for word in message.text.split(","):
-            add_keyword(word)
+            add_keyword(word.lstrip().rstrip())
     else:
-        add_keyword(message.text)
+        add_keyword(message.text.lstrip().rstrip())
     await message.answer("Успешно добавлено")
 
+@dp.callback_query_handler(lambda c: c.data == 'del_stop_keyword')
+async def process_callback_button1(call: types.CallbackQuery):
+    await Form2.del_stop_keyword.set()
+    await call.message.answer("Введите слово или несколько слов через запятую")
+
+@dp.message_handler(state=Form2.del_stop_keyword)
+async def process_name(message: types.Message, state: FSMContext):
+    await state.finish()
+    if "," in message.text:
+        for word in message.text.split(","):
+            await message.answer(del_keyword(word.lstrip().rstrip()))
+    else:
+        
+        await message.answer(del_keyword(message.text.lstrip().rstrip()))
